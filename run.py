c21bbd63c5086751ef9fe0cb59c1ce872bb75054
CHARAN121298 <gadhavi121298@gmail.com>
2024-06-18 12:49:12 +0100

    Dir Changed

--- run.py
+++ run.py
@@ -1,5 +1,33 @@
-from app import app
 import os
+from flask import Flask
+from flask import render_template, request, jsonify
+from model import  find_similar_movies
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+app = Flask(__name__)
+
+def create_movie_json(movie_titles):
+    movies_list = [
+        {"title": title, "poster":f"https://via.placeholder.com/250x375?text={title}" }
+        for title in movie_titles
+    ]
+    return movies_list
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/recommendations', methods=['POST'])
+def get_recommendations():
+    # Get the movie name from the POST request
+    movie_name = request.form.get('movie_name')
+
+    # Call the find_similar_movies function
+    titles = find_similar_movies(movie_name)
+
+    # Create JSON response
+    recommendation = create_movie_json(titles)
+
+    return jsonify(recommendation)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
